b30e9cd4db8883bfe59ea6a4c89976163fc0f34b
Peppe289 <gsperanza204@gmail.com>
2021-12-05 09:48:27 +0100

    hard update
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ function switchLang(){
         lang = 1;
         var nascondi = document.getElementsByClassName('eng');
         var mostra = document.getElementsByClassName('ita');
-        document.getElementById('pulsante').textContent="clicca per inglese"
         for(let i=0; i < nascondi.length; i++){
             nascondi[i].classList.add("hide");
             mostra[i].classList.remove("hide");
@@ -15,7 +14,6 @@ function switchLang(){
         lang = 0;
         var nascondi = document.getElementsByClassName('ita');
         var mostra = document.getElementsByClassName('eng');
-        document.getElementById('pulsante').textContent="click for italian"
         for(let i=0; i < nascondi.length; i++){
             nascondi[i].classList.add("hide");
             mostra[i].classList.remove("hide");
