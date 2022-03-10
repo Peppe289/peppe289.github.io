e969d04c2a9c20a7d85c0b284df01785cdf54c4a
Peppe289 <gsperanza204@gmail.com>
2022-03-10 18:58:12 +0100

    drop some stuff
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- priv/switch.js
+++
@@ -1,9 +0,0 @@
-function gotostep_2() {
-    var nascondi = document.getElementsByClassName('step-1');
-    var mostra = document.getElementsByClassName('step-2');
-    for(let i=0; i < nascondi.length; i++){
-        nascondi[i].classList.add("hide");
-        mostra[i].classList.remove("hide");
-    }
-    console.log("OPEN");
-}
