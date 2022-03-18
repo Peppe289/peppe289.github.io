32425fba9ee840987bb4c4412928ffb75ef6ea5d
Peppe289 <gsperanza204@gmail.com>
2022-03-18 08:14:23 +0100

    add single player mode
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- game/js-act/onclick.js
+++ game/js-act/onclick.js
@@ -4,6 +4,7 @@ var index_righe;
 var winner;
 var restart = 0;
 var single_player = 0;
+var errore;
 
 function enable_single_player() {
     if (single_player == 0) {
@@ -43,6 +44,61 @@ function restartgame(){
     console.log("OPEN");
 }
 
+function check_status() {
+    for (var i = 1; i != 10; i = i + 3)
+    {
+        if (document.getElementById("posto-" + i.toString()).innerHTML == "X" || document.getElementById("posto-" + i.toString()).innerHTML == "O") {
+            if (document.getElementById("posto-" + i.toString()).innerHTML == document.getElementById("posto-" + (i+1).toString()).innerHTML) {
+                if (document.getElementById("posto-" + (i+1).toString()).innerHTML == document.getElementById("posto-" + (i+2).toString()).innerHTML) {
+                    document.getElementById("status").innerHTML = "Tris";
+                }
+            }
+        }
+    }
+
+    for (var i = 1; i != 4; i++)
+    {
+        if (document.getElementById("posto-" + i.toString()).innerHTML == "X" || document.getElementById("posto-" + i.toString()).innerHTML == "O") {
+            if (document.getElementById("posto-" + i.toString()).innerHTML == document.getElementById("posto-" + (i+3).toString()).innerHTML) {
+                if (document.getElementById("posto-" + (i+3).toString()).innerHTML == document.getElementById("posto-" + (i+6).toString()).innerHTML) {
+                    document.getElementById("status").innerHTML = "Tris";
+                }
+            }
+        }
+    }
+
+    if (document.getElementById("posto-1").innerHTML == "X" || document.getElementById("posto-1").innerHTML == "O") {
+        if (document.getElementById("posto-1").innerHTML == document.getElementById("posto-5").innerHTML) {
+            if (document.getElementById("posto-5").innerHTML == document.getElementById("posto-9").innerHTML) {
+                document.getElementById("status").innerHTML = "Tris";
+            }
+        }
+    }
+
+    if (document.getElementById("posto-3").innerHTML == "X" || document.getElementById("posto-3").innerHTML == "O") {
+        if (document.getElementById("posto-3").innerHTML == document.getElementById("posto-5").innerHTML) {
+            if (document.getElementById("posto-5").innerHTML == document.getElementById("posto-7").innerHTML) {
+                document.getElementById("status").innerHTML = "Tris";
+            }
+        }
+    }
+
+    winner = document.getElementById("status").innerHTML;
+
+    var check_counter = 0;
+    for (var i = 0; i != 9; ++i) {
+        ++i;
+        let CheckID = "posto-" + i.toString();
+        if (document.getElementById(CheckID).innerHTML == "O" || document.getElementById(CheckID).innerHTML == "X")
+            check_counter++;
+        --i;
+    }
+    if (check_counter == 9)
+        winner = "end";
+
+    check_counter = 0
+}
+
 function checkinput() {
     var test = index_colonne;
     var pos = 0;
@@ -64,9 +120,11 @@ function checkinput() {
             //controlla se la posizione è già occupata
             let SelectID = "posto-" + input.toString();
             var _checkstatus = document.getElementById(SelectID).innerHTML;
-            if (_checkstatus == "X" || _checkstatus == "O")            
+            if (_checkstatus == "X" || _checkstatus == "O") {            
                 document.getElementById("status").innerHTML = "Errore nell'input";
-            else {
+                errore = 1;
+            } else {
+                errore = 0;
                 //gestisci i turni
                 turno++;
                 document.getElementById("status").innerHTML = "All good";
@@ -75,59 +133,7 @@ function checkinput() {
                 else
                     document.getElementById(SelectID).innerHTML = "X";
 
-
-                for (var i = 1; i != 10; i = i + 3)
-                {
-                    if (document.getElementById("posto-" + i.toString()).innerHTML == "X" || document.getElementById("posto-" + i.toString()).innerHTML == "O") {
-                        if (document.getElementById("posto-" + i.toString()).innerHTML == document.getElementById("posto-" + (i+1).toString()).innerHTML) {
-                            if (document.getElementById("posto-" + (i+1).toString()).innerHTML == document.getElementById("posto-" + (i+2).toString()).innerHTML) {
-                                document.getElementById("status").innerHTML = "Tris";
-                            }
-                        }
-                    }
-                }
-
-                for (var i = 1; i != 4; i++)
-                {
-                    if (document.getElementById("posto-" + i.toString()).innerHTML == "X" || document.getElementById("posto-" + i.toString()).innerHTML == "O") {
-                        if (document.getElementById("posto-" + i.toString()).innerHTML == document.getElementById("posto-" + (i+3).toString()).innerHTML) {
-                            if (document.getElementById("posto-" + (i+3).toString()).innerHTML == document.getElementById("posto-" + (i+6).toString()).innerHTML) {
-                                document.getElementById("status").innerHTML = "Tris";
-                            }
-                        }
-                    }
-                }
-
-                if (document.getElementById("posto-1").innerHTML == "X" || document.getElementById("posto-1").innerHTML == "O") {
-                    if (document.getElementById("posto-1").innerHTML == document.getElementById("posto-5").innerHTML) {
-                        if (document.getElementById("posto-5").innerHTML == document.getElementById("posto-9").innerHTML) {
-                            document.getElementById("status").innerHTML = "Tris";
-                        }
-                    }
-                }
-
-                if (document.getElementById("posto-3").innerHTML == "X" || document.getElementById("posto-3").innerHTML == "O") {
-                    if (document.getElementById("posto-3").innerHTML == document.getElementById("posto-5").innerHTML) {
-                        if (document.getElementById("posto-5").innerHTML == document.getElementById("posto-7").innerHTML) {
-                            document.getElementById("status").innerHTML = "Tris";
-                        }
-                    }
-                }
-
-                winner = document.getElementById("status").innerHTML;
-
-
-                var check_counter = 0;
-                for (var i = 0; i != 9; ++i) {
-                    ++i;
-                    let CheckID = "posto-" + i.toString();
-                    if (document.getElementById(CheckID).innerHTML == "O" || document.getElementById(CheckID).innerHTML == "X")
-                        check_counter++;
-                    --i;
-                }
-                if (check_counter == 9)
-                    winner = "end";
-                check_counter = 0
+                check_status();
             }
         }
     }
@@ -140,6 +146,8 @@ function checkinput() {
             mostra[i].classList.remove("hide");
         }
         console.log("OPEN");
+
+        return;
     } else {
         var nascondi = document.getElementsByClassName('open');
         var mostra = document.getElementsByClassName('close');
@@ -148,6 +156,31 @@ function checkinput() {
             mostra[i].classList.remove("hide");
         }
         console.log("OPEN");
+    }
+    
+    if (single_player == 1) {
+        var testing = 0;
+        turno++;
+        while (true && !errore) {
+            var pcMove = Math.floor(Math.random() * 10);
+            if (pcMove == 0)
+                continue;
+    
+            testing++;
+    
+            let SelectID = "posto-" + pcMove.toString();
+    
+            if (document.getElementById(SelectID).innerHTML != "X"
+                && document.getElementById(SelectID).innerHTML != "O") {
+                    if (turno % 2 == 0)
+                        document.getElementById(SelectID).innerHTML = "O";
+                    else
+                        document.getElementById(SelectID).innerHTML = "X";
+                break;        
+            } else if (testing == 20) {
+                break;
+            }
+        }
     }
 }
 
