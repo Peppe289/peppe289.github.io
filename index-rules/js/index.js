f7644097b2eb617b71a53ab2c2febbcd0b15333a
Peppe289 <gsperanza204@gmail.com>
2022-04-09 21:43:23 +0200

    hard update index
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

---
+++ index-rules/js/index.js
@@ -0,0 +1,71 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var isString = "Sono Peppe, uno studente e vivo in italia. Mi piace sviluppare software per ambiente Android e Linux. Molti di questi progetti sono opensource!";
+
+function SlowChangeBgColor(Element, rgb_r, rgb_g, rgb_b) {
+    document.getElementById(Element).style.backgroundColor = 
+    "rgb(" + rgb_r.toString() + "," + rgb_g.toString() + "," + rgb_b.toString() + ")";
+}
+
+function SlowChangeColor(Element, color) {
+    document.getElementById(Element).style.color = 
+    "rgb(" + color.toString() + "," + color.toString() + "," + color.toString() + ")";
+}
+
+function ShowById(isID) {
+    document.getElementById(isID).style.display = "block";
+}
+
+async function waitfor() {
+
+    for (let i = 0; i < 255; i++) {
+        SlowChangeColor("title", i, i, i);
+        await sleep(10);
+    }
+
+    for (let i = 0; i < isString.length; i++) {
+        document.getElementById("wellcome").innerHTML = document.getElementById("wellcome").innerHTML + isString[i];
+        await sleep(10);
+    }
+
+    // configura
+    var color = 0;
+    var rgb_r = 0;
+    var rgb_g = 0;
+    var rgb_b = 0;
+    //rgb(15, 76, 100)
+    for (let i = 0; i < 255; i++) {
+        if (rgb_r < 15)
+            rgb_r = i;
+
+        if (rgb_g < 76)
+            rgb_g = i;
+
+        if (rgb_b < 100)
+            rgb_b = i;
+
+        SlowChangeBgColor("body", rgb_r, rgb_g, rgb_b);
+        SlowChangeBgColor("main", i, i, i);
+        color = 255 - i;
+        SlowChangeColor("wellcome", color, color, color);
+        SlowChangeColor("title", color, color, color);
+        await sleep(10);
+    }
+
+    await sleep(200);
+    ShowById("navbar");
+    await sleep(200);
+    document.getElementById("main-link").style.display = "inline-block";
+
+
+    var desc = document.getElementsByClassName("mini-box");
+
+    for (let i = 0; i < desc.length; i++) {
+        desc[i].classList.remove("hide");
+    }
+
+}
+
+waitfor();
