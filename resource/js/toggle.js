a006e268f545807aa2f097d79b472e2cd154b937
Peppe289 <gsperanza204@gmail.com>
2022-05-08 16:27:24 +0200

    update
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- resource/js/toggle.js
+++ resource/js/toggle.js
@@ -1,7 +1,9 @@
 const navMenu = document.querySelector("#navMenu");
+const navBar = document.querySelector("#navbar");
 const sideBar = document.querySelector("#sidebar");
 
 navMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active");
+    navBar.classList.toggle("active");
     sideBar.classList.toggle("active");
 });
