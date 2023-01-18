2d355cd29eab0b1a0d32808bf35e28b201baac23
Peppe289 <gsperanza204@gmail.com>
2023-01-18 20:51:12 +0100

    reborn
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- resource/js/toggle.js
+++
@@ -1,9 +0,0 @@
-const navMenu = document.querySelector("#navMenu");
-const navBar = document.querySelector("#navbar");
-const sideBar = document.querySelector("#sidebar");
-
-navMenu.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    navBar.classList.toggle("active");
-    sideBar.classList.toggle("active");
-});
